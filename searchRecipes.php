01bd9e23cc0c1923051f02884332ad05aaa3c8b1
Grace Kisly <gkisly@coreebusiness.com>
2021-11-29 16:20:22 -0500

    connect to db again yay

--- searchRecipes.php
+++ searchRecipes.php
@@ -6,6 +6,7 @@
 	if($stmt->prepare("select name, id, minutes,n_steps, description from ingreds_tab,tab1_2 where ingreds_tab.recipe_id = tab1_2.id AND ingreds_tab.ingredient like ?") or die(mysqli_error($con))) {
 		$searchString = '%' . $_GET['searchRecipe'] . '%';
 		$stmt->bind_param("s", $searchString);
+		echo $_GET['searchRecipe'];
 		$stmt->execute();
 		$stmt->bind_result($name, $id, $minutes, $n_steps, $description);
 		echo "<table border=1><th>name</th><th>id</th><th>minutes</th><th>number of steps</th><th>description</th>\n";
