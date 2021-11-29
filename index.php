bf593e3613b201074379ed12e21f28c5f3715620
Grace Kisly <gkisly@coreebusiness.com>
2021-11-29 14:05:08 -0500

    changed index file again

--- index.php
+++ index.php
@@ -1,30 +1,40 @@
 <?php 
-// require_once("library.php");
-switch (@parse_url($_SERVER['REQUEST_URI'])['path']) {    
-    case '/':                   // URL (without file name) to a default screen
-        require 'registration.php';
-        echo @parse_url($_SERVER['REQUEST_URI'])['path'];
-        echo "Case 1";
-        break;  
-    case '/registration.php':
-        require 'registration.php';
-        echo @parse_url($_SERVER['REQUEST_URI'])['path'];
-        echo "Case 2";
-        break;
-    case '/login.php':
-        require 'login.php';
-        echo @parse_url($_SERVER['REQUEST_URI'])['path'];
-        echo "Case 3";
-        break;
-    case '/main.php':
-        require 'main.php';
-        echo @parse_url($_SERVER['REQUEST_URI'])['path'];
-        echo "Case 4";
-        break;
-   default:
-      echo @parse_url($_SERVER['REQUEST_URI'])['path'];
-      echo "Case 6";
-      http_response_code(404);
-      exit('Not Found');
-}  
+require_once("library.php");
+if(isset($_POST['submit'])){
+		$username = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		// Hash a new password for storing in the database.
+		// The function automatically generates a cryptographically safe salt.
+		$hashPassword = password_hash($password,PASSWORD_DEFAULT);
+		
+		$sql="INSERT INTO users (username, password)
+		VALUES ('".$username."','".$hashPassword."')";
+		$result = mysqli_query($conn, $sql);
+		if($result)
+		{
+			echo "Registration successfully";
+			header("location: login.php");
+			exit;
+		} else {
+			echo "Oops! Something went wrong. Please try again later.";
+		}
+	}
+if(isset($_POST['haveAccount'])){
+		header("location: login.php");
+		exit;
+}
 ?>
+
+<h1>Registration</h1>
+<div>
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+	<input type="text" name="user_name" value="" placeholder="Username">
+	<input type="password" name="password" value="" placeholder="Password">
+	<button type="submit" name="submit">Submit</button>
+</form>
+
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+	<button id="haveAccount" name="haveAccount">Already have an account?</button>
+</form>
+</div>
